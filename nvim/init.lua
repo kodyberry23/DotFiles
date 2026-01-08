5e6f8e65f3a73bb83570addd6936261dbb3e056b
Kody Berry <kodyberry@Kodys-MacBook-Pro.local>
2026-01-08 15:59:40 -0500

    fix multi session issues

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 -- Disable matchit plugin (we use % for select-all, Helix-style)
 vim.g.loaded_matchit = 1
 
+-- Disable netrw (using nvim-tree and oil.nvim instead)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Set leader keys BEFORE loading keymaps
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
